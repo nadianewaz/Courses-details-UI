59cbf904cc7892dfadceef8b1cbc078afcfd78da
nadianewaz <aninditaahmed@proton.me>
2023-12-21 14:30:05 +0600

    banner component added

---
+++ src/app/component/banner/banner.js
@@ -0,0 +1,16 @@
+import '../banner/banner';
+
+export default function Banner() {                   
+  return (
+    
+    <>
+    <div className="bg-no-repeat" style={{backgroundImage:`url('/img/banner.jpg')`, width: '100%',
+      height: '600px'}}>
+        {/* <h1 className='font-bold text-3xl pt-20 pl-12'></h1>  */}
+ 
+    </div>
+    </>
+    
+    
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Banner from './component/banner/banner'
 
 export default function Home() {
   return (
     <>
-    
+   < Banner></Banner>
     
     
     
