5befe938092ca436085a03e12bb859b7b8be748d
nadianewaz <aninditaahmed@proton.me>
2023-12-21 17:19:20 +0600

    class component added

---
+++ src/app/class/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <>
+        <div className='bg-black-800 py-10'>
+            <h1 className='text-2xl spacing  pl-48'>Class Courses</h1>
+            <p className='pl-48 font-thin'> Be it English Grammar or Spoken English, Quranic Grammar or simply Spoken Arabic - this is how Arabic to English;
+              </p>
+            <p className='pl-48 font-thin'>Ten Minute School's Language Learning courses are there to solve all the problems of language learning.</p>
+        
+        </div>
+        
+       
+
+
+
+            
+        </>
+    );
+};
+
+export default page;
---
+++ src/app/skills/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <>
+
+
+
+        
+            
+        </>
+    );
+};
+
+export default page;
